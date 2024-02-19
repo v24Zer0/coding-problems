cc9a217223aa5d4f49c2846902f16f1a649333fb
v24Zer0 <vinbesse@gmail.com>
2024-02-19 19:46:49 +0200

    find highest altitude

---
+++ find_highest_altitude/Solution.java
@@ -0,0 +1,22 @@
+package find_highest_altitude;
+
+// There is a biker going on a road trip.
+// The road trip consists of n + 1 points at different altitudes.
+// The biker starts his trip on point 0 with altitude equal 0.
+// You are given an integer array gain of length n where gain[i] is the net gain in altitude
+// between points i and i + 1 for all (0 <= i < n).
+// Return the highest altitude of a point.
+
+public class Solution {
+    public int largestAltitude(int[] gain) {
+        int highest = 0;
+        int altitude = 0;
+
+        for(int i = 0; i < gain.length; i++) {
+            altitude += gain[i];
+            highest = Math.max(highest, altitude);
+        }
+
+        return highest;
+    }
+}
